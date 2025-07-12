ac6d3d0331c5ca6d9e64cd5c884dc36496f1bfaa
Kaushal-Gawali <gawalikaushal7@gmail.com>
2025-07-12 23:08:09 +0530

    Hooks

--- Namaste_React/src/components/Body.js
+++ Namaste_React/src/components/Body.js
@@ -1,54 +1,23 @@
 import Restaurant_Card from "./Restaurant_Card";
+import { useState } from "react";
 import resList from "../utils/mockData";
-
 
 const Body = () => {
 
-  let listOfRestaurants = [
-    {
-    info: {
-      id: "565580",
-      name: "KFC",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/d2e6497c-c00e-47ce-8fcb-e0e3617a9d7b_565580.JPG",
-      costForTwo: "₹400 for two",
-      cuisines: ["Fast Food", "Burgers", "Rolls & Wraps"],
-      avgRating: 3.5,
-    },
-  },
-  {
-    info: {
-      id: "565581",
-      name: "Dominos",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/d2e6497c-c00e-47ce-8fcb-e0e3617a9d7b_565580.JPG",
-      costForTwo: "₹400 for two",
-      cuisines: ["Fast Food", "Burgers", "Rolls & Wraps"],
-      avgRating: 4.3,
-    },
-  },
-  {
-    info: {
-      id: "565582",
-      name: "Pizzhut",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/d2e6497c-c00e-47ce-8fcb-e0e3617a9d7b_565580.JPG",
-      costForTwo: "₹400 for two",
-      cuisines: ["Fast Food", "Burgers", "Rolls & Wraps"],
-      avgRating: 4.1,
-    },
-  },
-  ];
+  // Local state variable - super powerful variable
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
+
 
   return (
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={()=>{
-          listOfRestaurants = listOfRestaurants.filter((res)=>{
+          const filteredList = listOfRestaurants.filter((res)=>{
             // Filter Logic here
            return res.info.avgRating > 4
           });
-          console.log(listOfRestaurants)
+          setlistOfRestaurants(filteredList);
         }}>
           Top rated Restaurant         
         </button>
